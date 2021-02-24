b2dddf49c57a1680ead5e58761f9cd38b5794b13
ivanalsina <ivanalsina@hotmail.com>
2021-02-24 18:38:53 +0100

    Added fields 'publish date' and 'revision'

--- gui/fields.py
+++ gui/fields.py
@@ -1,5 +1,5 @@
 
-DATA_FIELDS = ['ISBN', 'Title', 'Author', 'Publisher', 'Subjects']
+DATA_FIELDS = ['ISBN', 'Title', 'Author', 'Publisher', 'Subjects', 'Publish date', 'Revision']
 LOC_FIELDS = ['Room', 'Module', 'Shelf']
 
 
@@ -9,6 +9,8 @@ JSON2FORM = {
     'authors' : 'Author',
     'publishers' : 'Publisher',
     'subjects' : 'Subjects',
+    'publish_date': 'Publish date',
+    'revision' : 'Revision',
     'room' : 'Room',
     'module' : 'Module',
     'shelf' : 'Shelf',
